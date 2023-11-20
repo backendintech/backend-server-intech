a22af67afba5fcfb413bf9ea36b579e3f5141b46
intechcompany <intechcompany.association@gmail.com>
2023-11-20 18:14:19 +0700

    fix cors

--- app.js
+++ app.js
@@ -1,55 +1,42 @@
-const express = require("express");
-const path = require("path");
-const cookieParser = require("cookie-parser");
-const logger = require("morgan");
-const notFoundMiddleware = require("./app/middleware/not-found");
-const handleErrorMiddleware = require("./app/middleware/handler-error");
-const usersRouter = require("./app/api/users/router");
-const authRouter = require("./app/api/auth/router");
-const productsRouter = require("./app/api/products/router");
-const categoryRouter = require("./app/api/category/router");
-const cors = require("cors");
+const express = require('express');
+const path = require('path');
+const cookieParser = require('cookie-parser');
+const logger = require('morgan');
+const notFoundMiddleware = require("./app/middleware/not-found")
+const handleErrorMiddleware = require("./app/middleware/handler-error")
+const usersRouter = require("./app/api/users/router")
+const authRouter = require("./app/api/auth/router")
+const productsRouter = require("./app/api/products/router")
+const categoryRouter = require("./app/api/category/router")
 
-const { authenticateUser } = require("./app/middleware/auth");
+const cors = require("cors")
+
+const {authenticateUser} = require("./app/middleware/auth")
+
+
 
 const app = express();
-const allowedOrigins = [
-  "http://localhost:5137",
-  "https://intech-dashboard-admin.vercel.app",
-];
 
-const corsOptions = {
-  origin: function (origin, callback) {
-    // Check if the request origin is included in the allowedOrigins array
-    if (!origin || allowedOrigins.includes(origin)) {
-      callback(null, true);
-    } else {
-      callback(new Error("Not allowed by CORS"));
-    }
-  },
-  methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
-  credentials: true,
-  optionsSuccessStatus: 204,
-};
+app.use(cors({
+    origin : "*"
+}))
 
-app.use(cors(corsOptions));
+const prefix = '/api'
 
-const prefix = "/api";
-
-app.use(logger("dev"));
+app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, "public")));
+app.use(express.static(path.join(__dirname, 'public')));
 
-app.get("/", async (req, res, next) => {
-  return res.send("hello world");
-});
+app.get("/", async (req,res,next) => {
+    return res.send("hello world")
+})
 
-app.use(`${prefix}/users`, usersRouter);
-app.use(`${prefix}/auth`, authRouter);
-app.use(`${prefix}/products`, productsRouter);
-app.use(`${prefix}/categories`, categoryRouter);
+app.use(`${prefix}/users`,usersRouter)
+app.use(`${prefix}/auth`, authRouter)
+app.use(`${prefix}/products`, productsRouter)
+app.use(`${prefix}/categories`, categoryRouter)
 
 // middlewares
 app.use(notFoundMiddleware);
